b29615f5081922a2054b20a59486c797448361e4
Migara Karunarathne <migara1225@gmail.com>
2022-06-04 23:02:54 +0530

    remove unwanted code

--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,7 @@
-import Map from "./constants/Map";
-
 export default class Index {
 	constructor() {
 		this.loadComponents();
 	}
 
-	loadComponents() {
-		console.log("test");
-	}
+	loadComponents() {}
 }
